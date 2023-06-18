41d5dbd60b3d8cce70c6e49cea9fe764763f5e3a
Joko Prabowo <jokoprabowo4550@gmail.com>
2023-06-18 19:26:57 +0700

    refactor user model

--- db/migrations/20230613131612-create-user.js
+++ db/migrations/20230613131612-create-user.js
@@ -3,14 +3,9 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
       username: {
         allowNull: false,
+        primaryKey: true,
         type: Sequelize.STRING
       },
       fullname: {
